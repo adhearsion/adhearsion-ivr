753f5b9fc8a399425025ffc0a13a88652923b407
Ben Langfeld <ben@langfeld.me>
2015-05-01 11:56:57 -0300

    Fix RSpec deprecations

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,14 +2,14 @@ require 'adhearsion'
 require 'adhearsion-ivr'
 
 RSpec.configure do |config|
-  config.color_enabled = true
+  config.color = true
   config.tty = true
 
   config.mock_with :rspec
   config.filter_run :focus => true
   config.run_all_when_everything_filtered = true
 
-  config.backtrace_clean_patterns = [/rspec/]
+  config.backtrace_exclusion_patterns = [/rspec/]
 
   config.before do
     Punchblock.stub new_request_id: 'foo'
